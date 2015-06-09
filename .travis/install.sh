6c061d3c32ca32f26d5b2382fd1b0a3ae385da91
Remi Rampin <remirampin@gmail.com>
2015-06-09 13:50:09 -0400

    Travis: get rid of run_lines()

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,24 +1,18 @@
 #!/bin/sh
 
-run_lines(){
-    while read line; do echo "$line"; sh -c "$line" || exit $?; done
-}
+set -eux
 
 case "$TEST_MODE"
 in
     run_tests)
-        run_lines<<'EOF'
         if [ $TRAVIS_PYTHON_VERSION = "2.6" ]; then pip install unittest2; fi
         pip install werkzeug
         python setup.py install
-EOF
         ;;
     coverage)
-        run_lines<<'EOF'
         pip install coveralls
         pip install werkzeug
         python setup.py install
-EOF
         ;;
     check_style)
         pip install flake8
--- .travis/test.sh
+++ .travis/test.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-run_lines(){
-    while read line; do echo "$line"; sh -c "$line" || exit $?; done
-}
+set -eux
 
 case "$TEST_MODE"
 in
@@ -13,8 +11,6 @@ in
         coverage run --append --source=usagestats.py --branch tests/__main__.py
         ;;
     check_style)
-        run_lines<<'EOF'
         flake8 --ignore=E126 usagestats.py tests wsgi/usagestats_server.py
-EOF
         ;;
 esac
